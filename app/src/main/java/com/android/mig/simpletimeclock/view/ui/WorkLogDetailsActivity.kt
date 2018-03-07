4a3a5e2ca1a890685a47ee298dbbc2dbcccbe6d6
Miguel Saenz <msaenz424@gmail.com>
2018-03-06 20:22:30 -0800

    fix: size of view that displays breaks
    Increased height of RecyclerView to display breaks because it was hard
    to see when a new break was added

--- app/src/main/java/com/android/mig/simpletimeclock/view/ui/WorkLogDetailsActivity.kt
+++ app/src/main/java/com/android/mig/simpletimeclock/view/ui/WorkLogDetailsActivity.kt
@@ -1,5 +1,6 @@
 package com.android.mig.simpletimeclock.view.ui
 
+import android.app.ActionBar
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
